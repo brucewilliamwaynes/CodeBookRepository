6e015670da5dd9d775a87b1728cf928992a2434b
Anirban <anirban.mukherjee@jda.com>
2021-06-13 00:09:46 +0530

    MindNumbing Question and categorised as A :(

---
+++ src/CodeForces/A/A1485.java
@@ -0,0 +1,29 @@
+package A;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A1485 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // if(br.ready()){
+            int T = Integer.parseInt(br.readLine());
+            for(int t = 1; t <= T ; t++) {
+                String inputs[] = br.readLine().split(" ");
+                long A = Long.parseLong(inputs[0]);
+                long B = Long.parseLong(inputs[1]);
+                int countOne = 0;
+                if(B == 1){
+                    countOne++;
+                    B = B+1;
+                }
+                while(A > 0){
+                    A = A/B;
+                    countOne++;
+                }
+                System.out.println(count);
+            }
+        // }
+    }
+}
