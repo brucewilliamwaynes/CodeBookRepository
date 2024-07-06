c50c4574d7538bcdff3973a520aec2ff74aba1fa
brucewilliamwaynes <mukherjee.anirbanmukherjee@gmail.com>
2024-07-06 18:44:07 +0530

    Closing last chapter

--- src/CodeForces/CodeForces.java
+++ src/CodeForces/CodeForces.java
@@ -2,10 +2,23 @@
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
 
 /* Name of the class has to be "Main" only if the class is public. */
 public class CodeForces
 {
+    public class Person {
+        public int cost;
+        public int reach;
+        Person(int cost, int reach) {
+            this.cost = cost;
+            this.reach = reach;
+        }
+    } 
+
     public static void main (String[] args) throws java.lang.Exception
     {
         // your code goes here
@@ -15,48 +28,22 @@ public class CodeForces
             for(int t = 1; t <= T ; t++) {
                 String inputs[] = br.readLine().split(" ");
                 int n = Integer.parseInt(inputs[0]);
-                int m = Integer.parseInt(inputs[1]);
-                char inputArray[] = br.readLine().toCharArray(); //size n
-                String indexArray[] = br.readLine().split(" "); //size m
-                
-                char utilString[] = br.readLine().toCharArray(); //size m
-                Arrays.sort(utilString);
-                char helperString[] = new char[m];
-
-                int indexArr[]  = new int[m];
-
-                for(int idx = 0; idx < m; idx++) {
-                    indexArr[idx] = Integer.parseInt(indexArray[idx]);
-                }
-
-                Arrays.sort(indexArr);
-                
-                int idx = 0;
-                int helperIdx = 0;
-                
-                while(idx < m-1) {
-                    if(indexArr[idx] != indexArr[idx+1]) {
-                        helperString[idx] = utilString[helperIdx];
-                        helperIdx++;
+                int p = Integer.parseInt(inputs[1]);
+                String reach[] = br.readLine().split(" ");
+                String cost[] = br.readLine().split(" ");
+                List<Person> list = new ArrayList<Person>((Collection<? extends CodeForces.Person>) new Comparator<Person>() {
+                    @Override
+                    public int compare(Person a, Person b) {
+                        return Integer.compare(a.cost, b.cost);
                     }
-                    idx++;
-                }
-
-                helperString[idx] = utilString[helperIdx];
-
-                for(int i = 0; i < m; i++) {
-                    if(helperString[i] == '\u0000') {
-                        continue;
-                    } else {
-                        inputArray[indexArr[i]-1] = helperString[i];
-                    }
-                }
-
-                System.out.println(String.valueOf(inputArray));
+                });
                 
-
+                for(int idx = 0; idx < n; idx++) {
+                    Person p = new Person(Integer.parseInt(reach[idx]), Integer.parseInt(cost[idx]));
+                    list.add(p);
+                }                
             }
-        // }
+        // } br.close();
     }
 
     // private static int findGCD(int i, int j, Map<Integer, Integer> myMap) {
